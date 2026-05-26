c00f1fa60859cfefa98c87ad9bc8909c2da2e5cd
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2026-05-26 12:55:19 -0700

    gamestate finish

--- gamestate.py
+++ gamestate.py
@@ -1,20 +1,23 @@
 
+import time
 
+import math
 class GameState:
-    dist_from_goal: int
+    dist_from_start: int
     at_goal: bool
     score: int
+    nearest_enemy: int
     time: int
     lives: int
     level: int
 
-    def __init__(self, dist_from_goal: int, 
+    def __init__(self, dist_from_start: int, 
                        at_goal: bool, 
                        score: int, 
                        time:int, 
                        lives: int = 3, 
                        level:int = 1):
-        self.dist_from_goal = dist_from_goal
+        self.dist_from_start = dist_from_start
         self.at_goal = at_goal
         self.score = score
         self.time = time
@@ -22,5 +25,30 @@ class GameState:
         self.level = level
 
 
-def position_to_goal(x: int, y: int):
-    pass
+def get_enemy_positions(ram):
+    enemies = []
+
+    for i in range(5):
+        enemy_drawn = ram[0x0F + i]
+
+        if enemy_drawn:
+            enemy_x = ram[0x6E + i] * 0x100 + ram[0x87 + i]
+            enemy_y = ram[0xCF + i]
+
+            enemies.append((enemy_x, enemy_y))
+
+    if len(enemies) > 0:
+        print(enemies)
+        time.sleep(0.1)
+    return enemies
+
+def get_nearest_enemy(enemy_positions, mario_x, mario_y):
+
+    nearest_enemy = math.inf
+    for enemy_x, enemy_y in enemy_positions:
+        man_dist = abs(mario_x - enemy_x) + abs(mario_y - enemy_y)
+
+        if man_dist < nearest_enemy:
+            nearest_enemy = man_dist
+
+    return nearest_enemy
--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
-from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+import gamestate
+import time
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, COMPLEX_MOVEMENT)
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
 done = True
 for step in range(5000):
     if done:
         state = env.reset()
     state, reward, done, info = env.step(env.action_space.sample())
+
+    ram = env.unwrapped.ram
+
+    print(gamestate.get_nearest_enemy(gamestate.get_enemy_positions(ram), info["x_pos"], info["y_pos"]))
     env.render()
 
 env.close()
