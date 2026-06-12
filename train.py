109c9715b1160ccaabcdd4748742f35e501a820c
Hyrenal <h.chaloyan@gmail.com>
2026-06-11 23:19:18 -0700

    Fixed opposite rank order

--- train.py
+++ train.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run the genetic algorithm." \
     " Running with no arguments runs with default values.")
     parser.add_argument("--generations", type=int, default=2000)
-    parser.add_argument("--population", type=int, default=40)
-    parser.add_argument("--steps", type=int, default=10000)
+    parser.add_argument("--population", type=int, default=20)
+    parser.add_argument("--steps", type=int, default=5000)
     args = parser.parse_args()
 
     GA = GeneticAlgorithm()
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     base = f"best_mario_{args.generations}g_{args.population}i_{args.steps}st"
     
     # Iterate through the top models
-    for rank, (model, score) in enumerate(GA.best_models, start=1):
+    for rank, (model, score) in enumerate(reversed(GA.best_models), start=1):
         
         # Add rank to base filename
         filepath = f"{base}_rank{rank}.pth"
